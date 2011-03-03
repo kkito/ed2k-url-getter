614820570a0b578c488a7cd64b2de9d4462bdc86
kk.shen <kk.shen@saybot.com>
2011-03-03 12:28:42 +0800

    update contents's innerhtml

--- content_script.js
+++ content_script.js
@@ -1,5 +1,7 @@
 var dbt = window.document.getElementById("downloadButton");
 if(!!dbt){
+    var cts = document.getElementById("contents");
+    if(!!cts) cts.innerHTML = dbt.name;
     alert(dbt.name);
 }else{
     alert("not found!");
